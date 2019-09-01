2a83aa3d40d4e9ec80374ad38c7c2743b1b1a02e
Fernando Ramirez <42774948+FernandoRamirez22@users.noreply.github.com>
2019-09-01 19:25:41 -0400

    CarProject
    My computer keeps glitching when trying to import to my project to GitHub, this same thing was happening with the Hello world project I am going to bring it in to either the IT department at the school or one of your TA's for help Wednesday.

---
+++ Chassis.java
@@ -0,0 +1,9 @@
+package sample;
+
+public interface Chassis {
+    //public fields
+    public String chassis = "Chassis";
+    public interface getChassisType();
+    public void setChassisType(String vehicleChassis);
+
+}
